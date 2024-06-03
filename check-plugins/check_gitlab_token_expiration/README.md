f5093416951bbc4c5ddb6f4c85b0b1ea17ad9427
beechesII <grau.christopher@googlemail.com>
2024-06-03 15:05:48 +0200

    change arguments from project_id,user_id and group_id to arguments scope and id

--- check-plugins/check_gitlab_token_expiration/README.md
+++ check-plugins/check_gitlab_token_expiration/README.md
@@ -1,12 +1,22 @@
 # Check Gitlab Token Expiration
 
-This script is an icinga2 monitoring check to check when a Gitlab token will expire.
+This script is an icinga2 monitoring check to check when a personal access token, a project access token or a group access will expire.
 
 ```
-usage: check_gitlab_token_expiration.py [-h] --url URL --private_token
-                                        PRIVATE_TOKEN --user_id USER_ID
-                                        --token_name TOKEN_NAME [--warn WARN]
-                                        [--crit CRIT]
+usage: check_gitlab_token_expiration.py [-h] --url URL --private_token PRIVATE_TOKEN --scope {user,project,group} --id ID --token_name TOKEN_NAME [--warn WARN] [--crit CRIT]
+
+check when a personal access token, a project access token or a group access token will expire
+
+options:
+  -h, --help            show this help message and exit
+  --url URL
+  --private_token PRIVATE_TOKEN
+  --scope {user,project,group}
+                        Scope for access token
+  --id ID               User ID, Project ID or Group ID
+  --token_name TOKEN_NAME
+  --warn WARN
+  --crit CRIT
 ```
 
 # Authors
--- check-plugins/check_gitlab_token_expiration/check_gitlab_token_expiration.py
+++ check-plugins/check_gitlab_token_expiration/check_gitlab_token_expiration.py
@@ -3,7 +3,7 @@
 """
 Copyright 2023-2024 Deutsche Telekom MMS GmbH
 Maintainer: Christopher Grau
-check when a private token will expire
+check when a personal access token, a project access token or a group access token will expire
 """
 
 import argparse
@@ -12,17 +12,24 @@ from datetime import datetime
 import gitlab
 
 parser = argparse.ArgumentParser(
-  prog = "check_gitlab_token_expiration.py"
-  )
+  prog = "check_gitlab_token_expiration.py",
+  description = "check when a personal access token,a project access token or a group access token will expire"
+)
 
 parser.add_argument('--url', required=True)
 parser.add_argument('--private_token',required=True)
-parser.add_argument('--user_id', default=None)
-parser.add_argument('--project_id', default=None)
-parser.add_argument('--group_id', default=None)
-parser.add_argument('--token_name',required=True)
-parser.add_argument('--warn',default=10)
-parser.add_argument('--crit',default=5)
+parser.add_argument('--scope',
+        choices=['user','project','group'],
+        required=True,
+        help='Scope for access token'
+)
+parser.add_argument('--id',
+        required=True,
+        help='User ID, Project ID or Group ID'
+)
+parser.add_argument('--token_name', required=True)
+parser.add_argument('--warn', default=10)
+parser.add_argument('--crit', default=5)
 
 args = parser.parse_args()
 
@@ -33,15 +40,12 @@ except gitlab.GitlabAuthenticationError:
     print('login with private token failed')
     sys.exit(255)
 
-if args.user_id != None:
-    access_tokens = gl.personal_access_tokens.list(user_id=args.user_id, lazy=True, active=True)
-elif args.project_id != None:
-    access_tokens = gl.projects.get(args.project_id, lazy=True, active=True).access_tokens.list()
-elif args.group_id != None:
-    access_tokens = gl.groups.get(args.group_id, lazy=True, active=True).access_tokens.list()
-else:
-    print('Please define on of these arguments: --user_id, --project_id, --group_id')
-    sys.exit(255)
+if args.scope == 'user':
+    access_tokens = gl.personal_access_tokens.list(user_id=args.id, lazy=True, active=True)
+elif args.scope == 'project':
+    access_tokens = gl.projects.get(args.id, lazy=True, active=True).access_tokens.list()
+elif args.scope == 'group':
+    access_tokens = gl.groups.get(args.id, lazy=True, active=True).access_tokens.list()
 
 # get expires_at of access_token with the name args.token_name
 for token in access_tokens:
